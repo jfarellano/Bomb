63244160d92ec5fc503bed9b3f3c8910734ab493
JFA <jfarellano@uninorte.edu.co>
2017-05-12 23:33:18 -0500

    Final commit

--- src/bombs/Bombs.java
+++ src/bombs/Bombs.java
@@ -3,55 +3,28 @@ package bombs;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Random;
 import java.util.StringTokenizer;
 
 public class Bombs {
 
-//    public static void main(String[] args) throws IOException {
-//        System.out.println("Digite inicialmente la cantidad de estaciones en su red y posteriormente la cantidad de rieles que hay usando el siguiente formato: estaciones rieles.");
-//        System.out.println("Despues digite parejas de estaciones para indicar que el riel une a esas dos estaciones.");
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        StringTokenizer st = new StringTokenizer(br.readLine());
-    
-//        int n = Integer.parseInt(st.nextToken());
-//        int m = Integer.parseInt(st.nextToken());
-    
-//        Graph G = new Graph(n);
-//        for (int i = 0; i < m; i++) {
-//            st = new StringTokenizer(br.readLine());
-//            int a = Integer.parseInt(st.nextToken());
-//            int b = Integer.parseInt(st.nextToken());
-//            G.AgregarArista(a, b);
-//        }
-//        System.out.println("La estacion que debe bombardear es la estacion " + G.bomb() + " porque su pigeon value es " + G.pigeonValue(G.bomb()) + ".");
-//        new GraphViewer(G);
-//    }
-    
-    
-    public static void main(String[] args) throws IOException {        
-        Random r = new Random();
+    public static void main(String[] args) throws IOException {
+        System.out.println("Digite inicialmente la cantidad de estaciones en su red y posteriormente la cantidad de rieles que hay usando el siguiente formato: estaciones rieles.");
+        System.out.println("Despues digite parejas de estaciones para indicar que el riel une a esas dos estaciones.");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+    
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+    
         Graph G = new Graph(n);
-        boolean sw = true;
-        for (int i = 0; i < n * (n - 1); i++) {
-            sw = true;
-            while(sw){
-                int a = r.nextInt(n);
-                int b = r.nextInt(n);
-                if(a != b){
-                    G.AgregarArista(a, b);
-                    sw = false;
-                }
-            }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            G.AgregarArista(a, b);
         }
-
         int a = G.bomb();
-        System.out.println("La estacion que debe bombardear es la estacion " + a + " : " + G.pigeonValue(a) + " : " + G.grado(a));
-
-        //new GraphViewer(G);
+        System.out.println("La estacion que debe bombardear es la estacion " + a + " porque su pigeon value es " + G.pigeonValue(a) + ".");
     }
     
 }
--- src/bombs/Graph.java
+++ src/bombs/Graph.java
@@ -1,33 +1,20 @@
 package bombs;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.LinkedList;
 
 public class Graph {
 
-    int orden, aux , tamaño, compQty;
+    int orden, aux , tamaño;
     boolean[][] A;
     boolean[] visitado;
-    int[][] components;
-    ArrayList<Node> nodes;
-    LinkedList<Integer> visited, cutSet;
     
     public Graph(int orden) {
-        this.nodes = new ArrayList<Node>();
         this.orden = orden;
         this.visitado = new boolean[orden];
-        this.components = new int[orden + 1][orden + 1];
         this.A = new boolean[orden + 1][orden + 1];
-        this.visited = new LinkedList<>();
-        this.cutSet = new LinkedList<>();
-        this.compQty = 0;
         this.tamaño = 0;
         for (int i = 0; i < orden; i++) {
             Arrays.fill(A[i], false);
-            nodes.add(new Node(i));
         }
     }
 
@@ -48,197 +35,28 @@ public class Graph {
         }
     }
 
-    public int component(int v) {
-        if (visitado[v]) {
-            return 0;
-        }
-        visitado[v] = true;
-        components[compQty][aux] = v;
-        aux++;
-        for (int i = 0; i < orden; i++) {
-            if (A[v][i]) {
-                component(i);
-            }
-        }
-        return aux;
-    }
-
-    public void getComponents() {
-        compQty = 0;
-        Arrays.fill(visitado, false);
-        for (int i = 0; i < orden; i++) {
-            if (!visitado[i]) {
-                aux = 1;
-                components[compQty][0] = component(i);
-                compQty++;
-            }
-        }
-    }
-
     public int pigeonValue(int vertex) {
         int pV = 0;
         Arrays.fill(visitado, false);
-        for (int i = 1; i < components[getVComp(vertex)][0]; i++) {
-            if (!visitado[components[getVComp(vertex)][i]] && components[getVComp(vertex)][i] != vertex) {
-                Kappa(components[getVComp(vertex)][i], vertex);
+        for (int i = 0; i < orden; i++) {
+            if (!visitado[i] && i != vertex) {
+                Kappa(i, vertex);
                 pV++;
             }
         }
-        return pV + compQty - 1;
+        return pV;
     }
 
-    public int getVComp(int v) {
-        for (int i = 0; i < compQty; i++) for (int j = 1; j < components[i][0]; j++) if (components[i][j] == v) return i;
-        return 0;
-    }
 
     public int bomb() {
         int maxPV = 0, maxV = 0, s = 0;
-        setCutEdges();
-        if(cutSet.size() == 0) for(int i = 0; i < orden; i++) if(grado(i) > 0) return i;
-        sortCutSet();
-        for (int i = 0; i < cutSet.size(); i++) {
-            if (grado(cutSet.get(i)) > 1) {
-                s = pigeonValue(cutSet.get(i));
-                if(i < cutSet.size() - 1){
-                    if((grado(cutSet.get(i)) == s - compQty + 1|| s > (grado(cutSet.get(i + 1)) + compQty)) && s >= maxPV && grado(cutSet.get(i)) > grado(cutSet.get(i + 1))) return cutSet.get(i);
-                    if(maxPV >= grado(cutSet.get(i + 1)) + compQty) return maxV;
-                }
-                if (s > maxPV) {
-                    maxPV = s;
-                    maxV = cutSet.get(i);
-                }
+        for (int i = 0; i < orden; i++) {
+            s = pigeonValue(i);
+            if (s > maxPV) {
+                maxPV = s;
+                maxV = i;
             }
         }
         return maxV;
-    }
-    
-     public int bomb1() {
-        int maxPV = 0, maxV = 0, s = 0;
-        getComponents();
-        for (int i = 0; i < compQty; i++) {
-            if (components[i][0] != 1) {
-                setingUpTree(components[i][1], 0);
-                setingLowValues(components[i][1]);
-            }
-        }
-        sortCutSet();
-        //System.out.println("Size: " + cutSet.size());
-        for (int i = 0; i < cutSet.size(); i++) {
-            if (grado(cutSet.get(i)) > 1) {
-                //System.out.println(cutSet.get(i) + " : " + i + " : " + cutSet.size());
-                s = pigeonValue(cutSet.get(i));
-                if (s > maxPV) {
-                    maxPV = s;
-                    maxV = cutSet.get(i);
-                }
-            }
-        }
-        return maxV;
-}
-
-    public int grado(int v) {
-        int g = 0;
-        for (int i = 0; i < orden; i++) if (A[v][i]) g++;
-        return g;
-    }
-
-    public class Node {
-
-        private final int data;
-        int visitedNumber;
-        int lowNumberReacheable = -1;
-
-        public LinkedList<Integer> backEdges = new LinkedList<>();
-        public LinkedList<Integer> forwardEdges = new LinkedList<>();
-
-        private int x = 0, y = 0;
-
-        public Node(int data) {
-            this.data = data;
-        }
-
-        public void setCoords(int _x, int _y) {
-            x = _x;
-            y = _y;
-        }
-
-        public boolean isValid() {
-            return x != 0 && y != 0;
-        }
-
-        public int getData() {
-            return data;
-        }
-
-        public int getX() {
-            return x;
-        }
-
-        public int getY() {
-            return y;
-        }
-    }
-    
-    public void sortCutSet(){
-        Collections.sort(cutSet, new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                return grado(o2) - grado(o1);
-            }
-        });
-    }
-
-    //John's place!
-
-
-    public void setingUpTree(int vertex, int cont) {
-        visited.add(vertex);
-        nodes.get(vertex).visitedNumber = cont + 1;
-        int comp = getVComp(vertex);
-        for (int i = 1; i < components[comp][0]; i++) {
-            if (A[vertex][components[comp][i]]) {
-                if (!visited.contains(components[comp][i])) {
-                    nodes.get(vertex).forwardEdges.add(components[comp][i]);
-                    setingUpTree(components[comp][i], cont + 1);
-                } else nodes.get(vertex).backEdges.add(components[comp][i]);
-            }
-        }
-    }
-
-    public int setingLowValues(int vertex) {
-        int low = 9999;
-        if (nodes.get(vertex).lowNumberReacheable == -1) {
-            low = nodes.get(vertex).visitedNumber; //Rule 1
-            nodes.get(vertex).lowNumberReacheable = low;
-            for (Integer backEdge : nodes.get(vertex).backEdges) {
-                int lowBackEdge = nodes.get(backEdge).visitedNumber; //Rule 2
-                if (low > lowBackEdge) {
-                    low = lowBackEdge;
-                    nodes.get(vertex).lowNumberReacheable = low;
-                }
-            }
-            if (nodes.get(vertex).visitedNumber == 1) if (nodes.get(vertex).forwardEdges.size() >= 2) cutSet.add(vertex); //System.out.println("Vertex " + vertex + " is an articulation point...");
-            for (Integer forwardEdge : nodes.get(vertex).forwardEdges) {
-                int lowFordwardEdge = setingLowValues(forwardEdge);
-                if (lowFordwardEdge >= nodes.get(vertex).visitedNumber) if (!cutSet.contains(vertex) && nodes.get(vertex).visitedNumber != 1) cutSet.add(vertex);
-                if (low > lowFordwardEdge) {
-                    low = lowFordwardEdge; //Rule 3
-                    nodes.get(vertex).lowNumberReacheable = low;
-                }
-            }
-
-        }
-        return low;
-    }
-
-    public void setCutEdges() {
-        getComponents();
-        for (int i = 0; i < compQty; i++) {
-            if (components[i][0] != 1) {
-                setingUpTree(components[i][1], 0);
-                setingLowValues(components[i][1]);
-            }
-        }
     }
 }
